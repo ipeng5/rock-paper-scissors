061e7529eba9fa4710fe9fb0aa8356ec30fe59f4
ipeng5 <ypeng115@gmail.com>
2022-08-17 18:29:56 +0200

    Update UI

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let cScore = document.getElementById("cScore");
 let pScore = document.getElementById("pScore");
 let announceResult = document.getElementById("announceResult");
 const cImg = document.getElementById("cImg");
+const buttonRock = document.getElementById("rock")
+const buttonPaper = document.getElementById("paper")
+const buttonScissors = document.getElementById("scissors")
+cScore.textContent = 0;
+pScore.textContent = 0;
+
+buttonScissors.addEventListener("mouseup", () => {
+    playRound("Scissors", getComputerChoice())
+});
+buttonPaper.addEventListener("mouseup", () => {
+    playRound("Paper", getComputerChoice());
+});
+buttonRock.addEventListener("mouseup", () => {
+    playRound("Rock", getComputerChoice());
+});
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -16,43 +31,42 @@ function getComputerChoice() {
     }
 }
 
-const buttonRock = document.getElementById("rock").addEventListener("mouseup", () => {
-    playRound("Rock")
-});
-const buttonPaper = document.getElementById("paper").addEventListener("mouseup", () => {
-    playRound("Paper")
-});
-const buttonScissors = document.getElementById("scissors").addEventListener("mouseup", () => {
-    playRound("Scissors")
-});
-
-
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    if (userScore < 5 && computerScore < 5) {
-        cImg.src = "./img/" + computerSelection.toLowerCase() + ".png";
-        if (playerSelection === computerSelection) {
-            announceResult.textContent = (`It's a tie! ${playerSelection} vs. ${computerSelection}`);
-        } else if (playerSelection === "Rock" && computerSelection === "Paper" ||
-            playerSelection === "Paper" && computerSelection === "Scissors" ||
-            playerSelection === "Scissors" && computerSelection === "Rock") {
-            computerScore++;
-            announceResult.textContent = (`You lose! ${computerSelection} beats ${playerSelection}`);
-            cScore.textContent = computerScore;
-        } else {
-            userScore++;
-            announceResult.textContent = (`You win! ${playerSelection} beats ${computerSelection}`);
-            pScore.textContent = userScore;
+function playRound(playerSelection, computerSelection) {
+    cImg.src = "./img/" + computerSelection.toLowerCase() + ".png";
+    if (playerSelection === computerSelection) {
+        announceResult.textContent = (`It's a tie! ${playerSelection} versus ${computerSelection}`);
+    } else if (playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Scissors" && computerSelection === "Rock") {
+        computerScore++;
+        announceResult.textContent = (`You lose! ${computerSelection} beats ${playerSelection}`);
+        cScore.textContent = computerScore;
+        if (computerScore === 5) {
+            computerWin();
         }
-    } else { endGame() }
+    } else {
+        userScore++;
+        announceResult.textContent = (`You win! ${playerSelection} beats ${computerSelection}`);
+        pScore.textContent = userScore;
+        if (userScore === 5) {
+            playerWin();
+        }
+    }
 }
 
-function endGame() {
-    if (userScore === 5) {
-        announceResult.textContent = ("Congratulations! You won the game!")
-    } if (computerScore === 5) {
-        announceResult.textContent = ("You lost the game :(");
-    } announceResult.style.color = "#FF6666";
+function playerWin() {
+    announceResult.textContent = ("Congratulations! You won the game!");
+    announceResult.style.color = "#FF6666";
+    buttonRock.disabled = true;
+    buttonPaper.disabled = true;
+    buttonScissors.disabled = true;
+}
+function computerWin() {
+    announceResult.textContent = ("Sorry. You just lost the game :(");
+    announceResult.style.color = "#FF6666";
+    buttonRock.disabled = true;
+    buttonPaper.disabled = true;
+    buttonScissors.disabled = true;
 }
 
 const reset = document.getElementById("reset")
@@ -63,14 +77,12 @@ reset.addEventListener("mouseup", () => {
 function newGame() {
     userScore = 0;
     computerScore = 0;
+    cScore.textContent = 0;
+    pScore.textContent = 0;
+    cImg.src = "img/q-mark.png";
     announceResult.textContent = ("Click icon below to choose your weapon");
+    announceResult.style.color = "#fff";
+    buttonRock.disabled = false;
+    buttonPaper.disabled = false;
+    buttonScissors.disabled = false;
 }
-
-
-
-
-
-
-
-
-
